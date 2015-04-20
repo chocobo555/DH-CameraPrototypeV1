a822f416eb4608813c54d933d3a7caf8a830921c
chocobo555 <ian.harris1@T3600-ENGINEER.iam.local>
2015-04-20 18:58:43 -0500

    more base particle stuff done

--- DarkeningHorizonsCameraV2/Assets/scripts/DarknessCrawl.cs
+++ DarkeningHorizonsCameraV2/Assets/scripts/DarknessCrawl.cs
@@ -1,15 +1,39 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class DarknessCrawl : MonoBehaviour {
+public class DarknessCrawl : MonoBehaviour 
+{
+	GameObject physicalCharacter;
+	bool isChasing = false;
+	public float darknessSpeed;
+
 
 	// Use this for initialization
-	void Start () {
-	
+	void Start () 
+	{
+		physicalCharacter = GameObject.Find("PhysicalCharacter");
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void Update () 
+	{
+		if (isChasing == true) 
+		{
+			float step = darknessSpeed * Time.deltaTime;
+			gameObject.transform.position = Vector3.MoveTowards(gameObject.transform.position, physicalCharacter.transform.position, step);
+		}
 	}
+
+	void OnTriggerEnter()
+	{
+		print("bloop");
+		isChasing = true;
+	}
+
+	void Chase()
+	{
+
+	}
+
+
 }
