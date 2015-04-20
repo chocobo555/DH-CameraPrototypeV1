6a667b0f7b93910e4e64b9839751b6abfe662738
chocobo555 <ian.harris1@loop.colum.edu>
2015-04-20 00:00:59 -0400

    created basic camera and physical character

---
+++ DarkeningHorizonsCameraV2/Assets/scripts/CameraFollow.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour 
+{
+
+	public GameObject physicalCharacter;
+	Vector3 myPos;
+
+
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+
+
+	// Update is called once per frame
+	void Update () 
+	{
+
+		gameObject.transform.position = physicalCharacter.transform.position;
+
+	}
+}
---
+++ DarkeningHorizonsCameraV2/Assets/scripts/movement.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class movement : MonoBehaviour 
+{
+
+	public float speed = 10;
+
+
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		float translationV = Input.GetAxis("Vertical") * speed; 
+		float translationH = Input.GetAxis("Horizontal") * speed; 
+
+		translationV *= Time.deltaTime;
+		translationH *= Time.deltaTime;
+
+		transform.Translate(translationH, 0, translationV);
+	}
+}
