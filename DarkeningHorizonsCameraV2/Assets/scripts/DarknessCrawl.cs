a38c21199d71a58f9578cb42e4d0f80c4d466cbe
chocobo555 <ian.harris1@loop.colum.edu>
2015-04-20 22:32:23 -0500

    OnBecomeVisible functions

--- DarkeningHorizonsCameraV2/Assets/scripts/DarknessCrawl.cs
+++ DarkeningHorizonsCameraV2/Assets/scripts/DarknessCrawl.cs
@@ -4,7 +4,8 @@ using System.Collections;
 public class DarknessCrawl : MonoBehaviour 
 {
 	GameObject physicalCharacter;
-	bool isChasing = false;
+	bool inRange = false;
+	bool inView = false;
 	public float darknessSpeed;
 
 
@@ -13,27 +14,51 @@ public class DarknessCrawl : MonoBehaviour
 	{
 		physicalCharacter = GameObject.Find("PhysicalCharacter");
 	}
+
 	
 	// Update is called once per frame
 	void Update () 
 	{
-		if (isChasing == true) 
+		//if (inRange == true)
+			//print ("inRange true");
+
+		//if (inView == true)
+			//print ("inView true");
+
+		/*
+		if (inRange == true && inView == false)
 		{
+			print ("yeah!!");
 			float step = darknessSpeed * Time.deltaTime;
 			gameObject.transform.position = Vector3.MoveTowards(gameObject.transform.position, physicalCharacter.transform.position, step);
 		}
+		*/
 	}
+
+
+	void OnWillRenderObject()
+	{
+		if(Camera.current.name == "LeftEyeAnchor" || Camera.current.name == "RightEyeAnchor")
+		{
+			print ("yeah");
+			inView = true;
+		}
+	}
+	/*
+	void OnBecameInvisible()
+	{
+		inView = false;
+	}
+	*/
+
 
 	void OnTriggerEnter()
 	{
-		print("bloop");
-		isChasing = true;
+		inRange = true;
 	}
-
-	void Chase()
+	void OnTriggerExit()
 	{
-
+		inRange = false;
 	}
-
 
 }
